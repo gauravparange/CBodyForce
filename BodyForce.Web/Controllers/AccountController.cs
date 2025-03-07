d3387675dd7510a79a42cfa4a71123eed7b04f9c
gaurav.parange <gaurav.parange@pmechtech.com>
2025-03-07 16:16:45 +0530

    7-3-2025 bodyforce accountcontroller login,signup,footer,header,layout

---
+++ BodyForce.Web/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BodyForce.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("signup")]
+        public async Task<IActionResult> SignUp()
+        {
+            return View();
+        }
+        [Route("login")]
+        public async Task<IActionResult> LogIn()
+        {
+            return View();
+        }
+    }
+}
